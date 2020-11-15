b809885eee16797dc4e18768c02a175ffbc1b594
Mariam-maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2020-11-15 13:17:00 +0200

    Q2

--- Student.java
+++ Student.java
@@ -3,5 +3,10 @@ package CMPproject;
 public class Student {
     String name;
     int roll_no;
-
+    int phoneNo;
+    String address;
+    public void DataOFStudent(){
+        System.out.println("Name Roll_NO PhoneNO Address");
+        System.out.println(name + "\t" + roll_no + "\t" + phoneNo + "\t" + address);
+    }
 }
--- TestLClass.java
+++ TestLClass.java
@@ -3,8 +3,18 @@ package CMPproject;
 public class TestLClass {
     public static void main(String[] args) {
         Student s1 = new Student();
+        Student s2 = new Student();
 
-        s1.name = "John";
-        s1.roll_no= 2;
+        s1.name="Sam";
+        s1.roll_no= 1;
+        s1.address="kkkk";
+        s1.phoneNo=523678;
+        s2.name = "John";
+        s2.roll_no= 2;
+        s2.address="hhhhh";
+        s2.phoneNo=38976;
+
+       s1.DataOFStudent();
+       s2.DataOFStudent();
     }
 }
